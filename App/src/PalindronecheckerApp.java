e58c934d553370599d024049d7bc63e50d0d7653
Chaithanya150806 <chaithanyakchaithanyak8@gmail.com>
2026-03-10 15:12:43 +0530

    UC13: Performance Comparison

--- App/src/PalindronecheckerApp.java
+++ App/src/PalindronecheckerApp.java
@@ -1,46 +1,45 @@
-import java.util.Stack;
+import java.util.Scanner;
 
-// Interface
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-
-// Stack implementation
-class StackStrategy implements PalindromeStrategy {
-
-    public boolean check(String input) {
-
-        Stack<Character> stack = new Stack<>();
-
-        // Push characters into stack
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-
-        // Create reversed string
-        StringBuilder reversed = new StringBuilder();
-
-        while (!stack.isEmpty()) {
-            reversed.append(stack.pop());
-        }
-
-        // Compare strings
-        return input.equals(reversed.toString());
-    }
-}
-
-// Main class
-class PalindromeCheckerApp {
+class UseCase13PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "level";
+        Scanner sc = new Scanner(System.in);
 
-        PalindromeStrategy strategy = new StackStrategy();
+        System.out.print("Input: ");
+        String input = sc.nextLine();
 
-        boolean result = strategy.check(input);
+        // Start time
+        long startTime = System.nanoTime();
 
-        System.out.println("Input: " + input);
-        System.out.println("Is Palindrome? " + result);
+        boolean isPalindrome = checkPalindrome(input);
+
+        // End time
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + executionTime + " ns");
+
+        sc.close();
+    }
+
+    public static boolean checkPalindrome(String str) {
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
